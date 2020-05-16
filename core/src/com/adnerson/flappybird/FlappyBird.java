1efdd8704b113d3c031754daf34398b326795ca0
Anderson <andercosta87@gmail.com>
2020-05-16 03:22:03 -0300

    animação

--- core/src/com/adnerson/flappybird/FlappyBird.java
+++ core/src/com/adnerson/flappybird/FlappyBird.java
@@ -9,10 +9,12 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class FlappyBird extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Texture fundo;
-	private Texture passaro1;
-	private Texture passaro2;
-	private Texture passaro3;
-	private int movimento=0;
+	private Texture[] passaros;
+
+	//*Configuração*//
+	private int larguraDispositivo;
+	private int alturaDispositivo;
+	private int variacao=0;
 
 
 
@@ -22,19 +24,24 @@ public class FlappyBird extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		fundo = new Texture("fundo.png");
-		passaro1 = new Texture("passaro1.png");
-		passaro2= new Texture("passaro2.png");
-		passaro3 = new Texture("passaro3.png");
+		passaros = new Texture[3];
+		passaros[0] = new Texture ("passaro1.png");
+		passaros[1] = new Texture ("passaro2.png");
+		passaros[2] = new Texture ("passaro3.png");
 
+
+		larguraDispositivo =  Gdx.graphics.getWidth();
+		alturaDispositivo =  Gdx.graphics.getHeight();
 
 	}
 
 	@Override
 	public void render () {
-		movimento++;
+		variacao++;
+		if(variacao>2) variacao =0;
 		batch.begin();
-		batch.draw(fundo,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		batch.draw(passaro1,movimento,400);
+		batch.draw(fundo,0,0,larguraDispositivo,alturaDispositivo);
+		batch.draw(passaros[variacao],30,alturaDispositivo/2);
 		batch.end();
 
 
